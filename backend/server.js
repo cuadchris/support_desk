0148028cc352b254d3617963d79ceab21affb02e
Christopher Cuadrado <cuadradochris@hotmail.com>
2022-12-31 14:14:08 -0500

    bringing in express and setting up .env

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
