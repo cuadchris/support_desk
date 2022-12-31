2477224f0ac8c2993faeb723e7371f9e3885f91c
Christopher Cuadrado <cuadradochris@hotmail.com>
2022-12-31 14:59:47 -0500

    async handler installed. custom error handler.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome" });
@@ -10,5 +14,7 @@ app.get("/", (req, res) => {
 
 //All routes from UserRoutes will have /api/users/ in front of it.
 app.use("/api/users", require("./routes/UserRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
