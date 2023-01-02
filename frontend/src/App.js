0f75fe978243aacd8914898e287220f41f6742a7
Christopher <cuadradochris@hotmail.com>
2023-01-02 12:19:43 -0500

    initial pages set up. navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div>My App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
