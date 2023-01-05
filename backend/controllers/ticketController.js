9dbba4d532dba9911c22554437336002e7e89241
Christopher <cuadradochris@hotmail.com>
2023-01-05 10:51:14 -0500

    ticket can now be created and saved into db.

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,45 @@ const Ticket = require("../models/ticketModel");
 // @route GET/api/tickets
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get tickets" });
+  //Get user from the ID in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Eroor("User not found");
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // @desc Create a ticket
 // @route POST/api/tickets
 // @access Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "create a ticket" });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("Please add a product and description");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Eroor("User not found");
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: "new",
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = {
--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -10,7 +10,7 @@ const ticketSchema = mongoose.Schema(
     product: {
       type: String,
       required: [true, "Please select a product"],
-      enum: ["iphone", "Macbook Pro", "iMac", "iPad"],
+      enum: ["iPhone", "Macbook Pro", "iMac", "iPad"],
     },
     description: {
       type: String,
