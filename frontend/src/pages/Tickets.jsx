fec77ea7fbe9bc8c8a81807e42e4de516aab7006
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-01-07 18:20:12 -0500

    single ticket component

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
+import TicketItem from "../components/TicketItem";
 import { getTickets, reset } from "../features/tickets/ticketSlice";
 
 const Tickets = () => {
@@ -27,9 +29,21 @@ const Tickets = () => {
   }
 
   return (
-    <div>
+    <>
+      <BackButton url={"/"} />
       <h1>Tickets</h1>
-    </div>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
   );
 };
 export default Tickets;
